c40ac8a46df6c59019e09b019de47e01838c9ac9
Denis <148818867+Sunofu@users.noreply.github.com>
2024-10-10 01:55:31 +0300

    0.0.8.5

---
+++ Plugins/Marketplace/FlatNodes/Intermediate/Build/Mac/UnrealEditor/Inc/FlatNodes/UHT/FlatNodesSettings.generated.h
@@ -0,0 +1,66 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "FlatNodesSettings.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FLATNODES_FlatNodesSettings_generated_h
+#error "FlatNodesSettings.generated.h already included, missing '#pragma once' in FlatNodesSettings.h"
+#endif
+#define FLATNODES_FlatNodesSettings_generated_h
+
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_SPARSE_DATA
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_ACCESSORS
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUFlatNodesSettings(); \
+	friend struct Z_Construct_UClass_UFlatNodesSettings_Statics; \
+public: \
+	DECLARE_CLASS(UFlatNodesSettings, UDeveloperSettings, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/FlatNodes"), NO_API) \
+	DECLARE_SERIALIZER(UFlatNodesSettings) \
+	static const TCHAR* StaticConfigName() {return TEXT("EditorPerProjectUserSettings");} \
+
+
+
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UFlatNodesSettings(UFlatNodesSettings&&); \
+	NO_API UFlatNodesSettings(const UFlatNodesSettings&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UFlatNodesSettings); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UFlatNodesSettings); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UFlatNodesSettings) \
+	NO_API virtual ~UFlatNodesSettings();
+
+
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_13_PROLOG
+#define FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_SPARSE_DATA \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_ACCESSORS \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_INCLASS_NO_PURE_DECLS \
+	FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h_16_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> FLATNODES_API UClass* StaticClass<class UFlatNodesSettings>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Build_U5M_Marketplace_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_FlatNodes_Source_FlatNodes_Public_FlatNodesSettings_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
